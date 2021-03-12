959733f0b4612a9fcc50c0880e28b6110483b5a7
jplukas <jplukasavicus@gmail.com>
2021-03-12 19:49:51 -0300

    Exclui arquivos estáticos

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,11 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 CKEDITOR_UPLOAD_PATH = 'uploads/'
-
-CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
 CKEDITOR_CONFIGS = {
     'default': {
