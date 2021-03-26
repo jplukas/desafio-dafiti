66f17b83349607d3d9950bd1d41e5210bf7e7fee
jplukas <joao.psilva@dafiti.com>
2021-03-25 23:14:34 -0300

    Adiciona form para Tag

--- blog/myblog/forms.py
+++ blog/myblog/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 
-from myblog.models import Post
+from myblog.models import Post, Tag
 
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['author', 'title', 'summary', 'content', 'tags']
+
+
+class TagForm(ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['name']
