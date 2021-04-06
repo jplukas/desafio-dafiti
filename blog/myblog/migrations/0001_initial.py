b3d07d45cea206ebfade6edc70058e775aefbbfb
jplukas <jplukasavicus@gmail.com>
2021-04-06 10:36:49 -0300

    Arquivos formatados

--- blog/myblog/migrations/0001_initial.py
+++ blog/myblog/migrations/0001_initial.py
@@ -19,19 +19,38 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Tag',
             fields=[
-                ('name', models.CharField(max_length=31, primary_key=True, serialize=False)),
+                (
+                    'name',
+                    models.CharField(
+                        max_length=31, primary_key=True, serialize=False
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID',
+                    ),
+                ),
                 ('title', models.CharField(max_length=255)),
                 ('summary', ckeditor.fields.RichTextField()),
                 ('content', ckeditor_uploader.fields.RichTextUploadingField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL)),
+                (
+                    'author',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
                 ('tags', models.ManyToManyField(blank=True, to='myblog.Tag')),
             ],
         ),
